da19e486f3b1cd949212cce2b24c1e48b9ec069d
saurabh1mohite <42999582+saurabh1mohite@users.noreply.github.com>
2021-12-05 12:12:54 -0700

    Update core.py

--- src/core.py
+++ src/core.py
@@ -92,14 +92,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.command == 'preprocess':
-    preprocess(
-        args.dataroot, args.force, args.split, args.split_name, args.width,
-        args.height)
-    elif args.command == 'train':
-        train(args)
-    elif args.command == 'test':
-        test(args)
-    elif args.command == 'watch':
-        watch(args)
-    else:
-        pass
+        preprocess(
+            args.dataroot, args.force, args.split, args.split_name, args.width,
+            args.height)
+        elif args.command == 'train':
+            train(args)
+        elif args.command == 'test':
+            test(args)
+        elif args.command == 'watch':
+            watch(args)
+        else:
+            pass
